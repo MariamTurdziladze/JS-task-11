ef49db188eb3a979c2b207aabb9e7101cd97a9d5
MariamTurdziladze <mariamt789@gmail.com>
2024-12-16 00:22:30 +0400

    edited
    added red borders and fixed js

--- script.js
+++ script.js
@@ -1,50 +1,57 @@
 document.getElementById('signup-form').addEventListener('submit', function (event) {
     event.preventDefault();
 
-let isValid = true;
+    let isValid = true;
 
-const firstName = document.getElementById('firstName').value;
-const firsNamePattern = /^[A-Za-z]{2,30}(-[A-Za-z]{2,30})?$/;
-const firsNameError = document.getElementById('firstName-error');
+    const firstNameInput = document.getElementById('firstName');
+    const firstNamePattern = /^[A-Za-z]{2,30}(-[A-Za-z]{2,30})?$/;
+    const firstNameError = document.getElementById('firstName-error');
 
-if (!firsNamePattern.test(firstName)) {
-    firsNameError.textContent = "First Name should contain 2-30 characters";
-    isValid = false;
-} else {
-    firsNameError.textContent = "";
-}
+    if (!firstNamePattern.test(firstNameInput.value)) {
+        firstNameError.textContent = "First Name should contain 2-30 characters";
+        firstNameInput.style.border = "1px solid red";
+        isValid = false;
+    } else {
+        firstNameError.textContent = "";
+        firstNameInput.style.border = "";
+    }
 
-const lastName = document.getElementById('lastName').value;
-const lastNamePatter = /^[A-Za-z]{2,30}(-[A-Za-z]{2,30})?$/;
-const lastNameError = document.getElementById('lastName-error');
+    const lastNameInput = document.getElementById('lastName');
+    const lastNamePattern = /^[A-Za-z]{2,30}(-[A-Za-z]{2,30})?$/;
+    const lastNameError = document.getElementById('lastName-error');
 
-if (!lastNamePatter.test(lastName)) {
-    lastNameError.textContent = "Last Name should contain 2-30 characters";
-    isValid = false;
-} else {
-    lastNameError.textContent = "";
-}
+    if (!lastNamePattern.test(lastNameInput.value)) {
+        lastNameError.textContent = "Last Name should contain 2-30 characters";
+        lastNameInput.style.border = "1px solid red";
+        isValid = false;
+    } else {
+        lastNameError.textContent = "";
+        lastNameInput.style.border = "";
+    }
 
-const email = document.getElementById('email').value;
-const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-const emailError = document.getElementById('email-error');
+    const emailInput = document.getElementById('email');
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const emailError = document.getElementById('email-error');
 
-if (!emailPattern.test(email)) {
-    emailError.textContent = "Please enter valid email";
-    isValid = false;
-} else {
-    emailError.textContent = "";
-}
+    if (!emailPattern.test(emailInput.value)) {
+        emailError.textContent = "Please enter a valid email";
+        emailInput.style.border = "1px solid red";
+        isValid = false;
+    } else {
+        emailError.textContent = "";
+        emailInput.style.border = "";
+    }
 
-const password = document.getElementById('password').value;
-const passwordPattern = /^.{6,}$/;
-const passwordError = document.getElementById('password-error');
+    const passwordInput = document.getElementById('password');
+    const passwordPattern = /^.{6,}$/;
+    const passwordError = document.getElementById('password-error');
 
-if (!passwordPattern.test(password)) {
-    passwordError.textContent = "Please use at least 6 characters";
-    isValid = false;
-} else {
-    passwordError.textContent = "0;"
-}
-
-})
+    if (!passwordPattern.test(passwordInput.value)) {
+        passwordError.textContent = "Please use at least 6 characters";
+        passwordInput.style.border = "1px solid red";
+        isValid = false;
+    } else {
+        passwordError.textContent = "";
+        passwordInput.style.border = "";
+    }
+});
